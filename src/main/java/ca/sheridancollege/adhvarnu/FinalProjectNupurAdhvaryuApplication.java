a8b044ab4c0975ee7922a53eba8f7e9aec88ea50
adhvarnu <adhvarnu@sheridancollege.ca>
2021-04-11 20:30:18 -0400

    Starter Project

---
+++ src/main/java/ca/sheridancollege/adhvarnu/FinalProjectNupurAdhvaryuApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.adhvarnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectNupurAdhvaryuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectNupurAdhvaryuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/sheridancollege/adhvarnu/FinalProjectNupurAdhvaryuApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.adhvarnu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectNupurAdhvaryuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
